827df9ae50b23a3d7063dc0b2b87e99d7f67e93b
Yuji Adachi <66458951+YujiAdachi@users.noreply.github.com>
2026-01-18 06:05:08 +0000

    chore : create login

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Todo アプリ</h1>
+          <p>こんにちは、{user?.signInDetails?.loginId}さん！</p>
+          <button onClick={signOut}>サインアウト</button>
+        </main>
+      )}
+    </Authenticator>
   )
 }
 
